5097398d7f983aec3224c2476c983ff194e32736
PrashamsaS <prashamsashrestha28@gmail.com>
2024-11-19 17:50:13 +0545

    frontend

--- client/src/components/Top.jsx
+++ client/src/components/Top.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import homeimg from '../assets/homeee.png'
+import elipse1 from '../assets/Ellipse 1.png'
+import elipse2 from '../assets/Ellipse 2.png'
 import bg from '../assets/bg.png'
 
 const Top = () => {
   return (
+    
     <div className='imagee'>
-     
-      <img src={homeimg}className='bg'></img>
-      <button className='buttonnn'>Our Services</button>
+     <img src={elipse1}className='bg'></img>
+     <img src={elipse2}className='bg2'></img>
+      <img src={bg}className='bg1'></img>
+      <h1 className='text'>Caring for Life</h1>
+      <h1 className='text1'>Leading the Way
+      in Medical Excellence</h1>
+      <button className='topbutton'>Our Services</button>
+    <div ok>ok</div>
     </div>
   )
 }
